f4d0c3d3f93ee3324b953df1021a398853d8efff
Silac <59840327+Silac1995@users.noreply.github.com>
2026-05-07 14:24:47 +0200

    feat(furniture): add FurnitureTag.Cashier

--- Assets/Scripts/World/Furniture/FurnitureTag.cs
+++ Assets/Scripts/World/Furniture/FurnitureTag.cs
@@ -6,5 +6,6 @@ public enum FurnitureTag
     Crafting,   // Workshop
     Storage,    // Warehouse
     Seating,    // Living room / Tavern
-    TimeClock   // Punch-in / punch-out station for a CommercialBuilding
+    TimeClock,  // Punch-in / punch-out station for a CommercialBuilding
+    Cashier     // Customer-facing transaction counter for a ShopBuilding (or future BankBuilding etc.)
 }
